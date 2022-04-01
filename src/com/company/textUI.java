f2ed932c88b8b1cf8622afd2f6719edf07a324b4
HelenaLykstoft <99395714+HelenaLykstoft@users.noreply.github.com>
2022-04-01 10:29:34 +0200

    Created textUI and filled it

--- src/com/company/textUI.java
+++ src/com/company/textUI.java
@@ -1,10 +1,44 @@
 package com.company;
 
-public class textUI {
-    public void registerTeam(){
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class textUI {
+
+
+    public ArrayList<String> registerTeam(String message){
+        // Creating new ArrayList to create one team at a time
+        ArrayList<String> data = new ArrayList<>();
+        String input; // The input we write
+        Scanner scan = new Scanner(System.in); // Creating scanner so we can read the prompt
+        System.out.println("Write the teamname here: ");
+        input = scan.nextLine();
+        data.add(input);
+
+        while(data.size()<5){ // 5 because theres teamname and up to 5 players
+            System.out.println(message);
+
+            if(input.equalsIgnoreCase("Q")){
+                break;
+            }
+            data.add(input);
+        }
+        return data;
     }
-    public void showTeams(){
+    public void displayMessage(String msg){
+        System.out.println(msg);
+    }
+    public String getUserInput(String message){
+        displayMessage(message);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    // Creates a msg where we can write whatever we want to user
+    public void writeToUser(String msg){
+        System.out.println(msg);
+    }
+    /*public void showTeams(){
 
     }
     public void showResults(){
@@ -18,5 +52,5 @@ public class textUI {
     }
     public void showNextMatch(){
 
-    }
+    } Skal de bruges eller ej her? et andet sted? aldrig?*/
 }
