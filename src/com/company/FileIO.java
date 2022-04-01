3923e3c7b7b7c573e2665577518e838d27ef7500
solskinIsak <cph-mm712@cphbusiness.dk>
2022-04-01 11:47:40 +0200

    @Override toString in Team class

--- src/com/company/FileIO.java
+++ src/com/company/FileIO.java
@@ -28,4 +28,23 @@ public class FileIO {
         return teamData;
 
     }
+    public ArrayList<String> writeTeamData() {
+        return readTeamData();
+
+    }
+
+    public void saveData(Team team) {
+        String teamData = "teamName, playerNames, score, position\n";
+        for (String player : team.teamPlayers) {
+            teamData = "" + team.getTeamName() + ", " + team.getTeamPlayers() + ", " + team.getTeamPosition() + "\n";
+        }
+        try {
+            FileWriter output = new FileWriter("src/teamData.txt");
+            output.write(teamData);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
