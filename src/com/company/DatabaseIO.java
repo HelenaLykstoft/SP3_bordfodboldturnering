15d36d40fbcd6570e289756611680125ac5a883d
solskinIsak <cph-mm712@cphbusiness.dk>
2022-04-25 11:03:48 +0200

    redundant data file deleted

--- src/com/company/DatabaseIO.java
+++ src/com/company/DatabaseIO.java
@@ -4,31 +4,41 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class DatabaseIO {
+public class DatabaseIO implements IO {
     Connection connection = null;
 
     public void createConnection() {
 
         String JdbcUrl = "jdbc:mysql://127.0.0.1:3306/sp3+?" + "autoReconnect=true&useSSL=false";
         String username = "root";
-        String password = "Lampen04aug";
+        // String password = "Lampen04aug"; // Helenas Password
+        // String password = "Mysql1238Code18"; // Jamies Password
+        String password = "Solskin#12";
 
         try {
             connection = DriverManager.getConnection(JdbcUrl, username, password);
 
-            insertInfoToDB();
-            readInfoFromDB();
-            connection.close();
+
+
 
         } catch (SQLException e) {
             e.printStackTrace();
 
         }
     }
+    public void closeConnection()
+    {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void insertInfoToDB() {
-        String insertTeamName = "INSERT INTO team (name,score,teamPoints,goalPoints) VALUES ('SUPER','1','2','3')";
+        String insertTeamName = "INSERT INTO team (name,score,teamPosition,goalPoints) VALUES ('SUPER','1','2','3')";
         try {
             PreparedStatement query1 = connection.prepareStatement(insertTeamName);
             var query1Result = query1.executeUpdate();
@@ -51,5 +61,61 @@ public class DatabaseIO {
         } catch (SQLException b) {
             b.printStackTrace();
         }
+    }
+
+    @Override
+    public ArrayList<Team> readTeamData()
+    {
+        return null;
+    }
+
+    @Override
+    public void writeTeamData(ArrayList<Team> teams)
+    {
+        createConnection();
+        String teamData;
+        try {
+            PreparedStatement query1;
+            for (int i = 0; i < teams.size(); i++) {
+                teamData = "INSERT INTO team (name,score,teamPosition,goalPoints) VALUES ('" + teams.get(i).getTeamName() + "','" + teams.get(i).getTeamScore() + "','" + teams.get(i).getTeamPosition() + "','" + teams.get(i).getGoalPoints()+"')";
+                query1 = connection.prepareStatement(teamData);
+                var query1Result = query1.executeUpdate();
+                System.out.println(query1Result);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+
+    }
+
+    @Override
+    public void writeMatchData(ArrayList<Match> matches)
+    {
+        createConnection();
+        String matchData;
+        try {
+            PreparedStatement query1;
+            for (int i = 0; i < matches.size(); i++) { //startTime, endTime, team1, team2, matchNumber
+                matchData = "INSERT INTO match (startTime,endTime,team1,team2) VALUES ('" + matches.get(i).getStartTime() + "','" + matches.get(i).getEndTime() + "','" + matches.get(i).getTeam1() + "','" + matches.get(i).getTeam2()+"')";
+                query1 = connection.prepareStatement(matchData);
+                var query1Result = query1.executeUpdate();
+                System.out.println(query1Result);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+
+    }
+
+    @Override
+    public ArrayList<Match> readMatchData(ArrayList<Team> teams)
+    {
+        return null;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //Tournament tournament = new Tournament();
-        //tournament.tournamentMenu();
-        DatabaseIO test= new DatabaseIO();
-        test.createConnection();
+        Tournament tournament = new Tournament();
+        tournament.tournamentMenu();
+        //DatabaseIO test= new DatabaseIO();
+        //test.createConnection();
+
+
     }
 }
